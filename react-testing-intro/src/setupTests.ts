acbae5267129c1f5ccdea89be2dd36b811c0f4d6
Alvaro Dominguez Lopez <alvarodl20@outlook.com>
2020-03-28 23:39:41 +0100

    FEAT: Added Custom Snapshot Serializer.

--- react-testing-intro/src/setupTests.ts
+++ react-testing-intro/src/setupTests.ts
@@ -6,5 +6,9 @@ import '@testing-library/jest-dom/extend-expect';
 
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import { createSerializer } from 'enzyme-to-json';
+
+//@ts-ignore
+expect.addSnapshotSerializer(createSerializer({mode: 'deep'}));
 
 Enzyme.configure({ adapter: new Adapter() });
